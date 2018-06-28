29b68508167acdf942e4c8066ff2523a6fe7cad5
Jabari Dawkins <jabaridawkins@gmail.com>
2018-06-28 17:45:26 -0400

    Add correct build artifacts

---
+++ build/CMakeFiles/com.github.jabari-dawkins.elementary-hello-world.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello.c"
+  "com.github.jabari-dawkins.elementary-hello-world_valac.stamp"
+  "CMakeFiles/com.github.jabari-dawkins.elementary-hello-world.dir/src/hello.c.o"
+  "com.github.jabari-dawkins.elementary-hello-world.pdb"
+  "com.github.jabari-dawkins.elementary-hello-world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.jabari-dawkins.elementary-hello-world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
